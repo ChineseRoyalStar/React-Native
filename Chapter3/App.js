b32816ae5da311b2be821cbe2789d075ac347e28
Leon Guo <chineseroyalstar@gmail.com>
2018-08-31 00:46:40 +0800

    第3章 页面导航、弹出框及深入理解属性

---
+++ Chapter3/App.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {AppRegistry, Platform, BackHandler} from 'react-native';
+import LoginLeaf from './LoginLeaf';
+import WaitingLeaf from './WaitingLeaf';
+import { platform } from 'os';
+
+export default class NaviModule extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentScene: 'Login',
+      phoneNumber: '',
+      userPW: ''
+    };
+    this.handleBackSignal = this.handleBackSignal.bind(this);
+    this.onLoginPressed = this.onLoginPressed.bind(this);
+  }
+
+  onLoginPressed(aNumber, aPW) {
+    this.setState ({
+      currentScene: 'Waiting',
+      phoneNumber: aNumber,
+      userPW: aPW
+    });
+  }
+
+  render() {
+      if ( this.state.currentScene === 'Login')
+        return <LoginLeaf onLoginPressed={this.onLoginPressed} />;
+      else return (
+          <WaitingLeaf phoneNumber={this.state.phoneNumber} onGobackPressed={this.handleBackSignal} userPW={this.state.userPW} />
+      );
+  }
+
+  handleBackSignal() {
+    if (this.state.currentScene === 'Waiting') {
+      this.setState({currentScene: 'Login'});
+      return true;
+    }
+    return false;
+  }
+
+  componentDidMount() {
+    if (Platform.OS === 'android')
+      BackHandler.addEventListener('hardwareBackPress', this.handleBackSignal);
+  }
+
+  componentWillUnmount() {
+    if (platform.OS === 'android')
+      BackHandler.removeEventListener('hardwareBackPress', this.handleBackSignal);
+  }
+}
+
+AppRegistry.registerComponent('LearnRN', () => NaviModule);
