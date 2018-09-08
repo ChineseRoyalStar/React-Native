a24fc8e8d6dc6f2562bb962a925d241fba290950
Leon Guo <chineseroyalstar@gmail.com>
2018-09-09 00:30:41 +0800

    第5章 flexbox布局、View、Image与可触摸组件: View组件的回调函数

--- Chapter5/LearnRN/App.js
+++ Chapter5/LearnRN/App.js
@@ -9,42 +9,30 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
-
 export default class App extends Component {
  
+  constructor(props) {
+    super(props);
+    this._onTouchMove = this._onTouchMove.bind(this);
+    this._onTouchStart = this._onTouchStart.bind(this);
+    this._onTouchEnd = this._onTouchEnd.bind(this);
+  }
+
+  _onTouchMove(event) {
+    console.log("touch move: " + event.thisStamp + ', X:' + event.nativeEvent.locationX + ', Y' + event.nativeEvent.locationY);
+  }
+
+  _onTouchStart(event) {
+    console.log("touch start: " + event.thisStamp + ', X:' + event.nativeEvent.locationX + ', Y' + event.nativeEvent.locationY);
+  }
+
+  _onTouchEnd(event) {
+    console.log("touch end: " + event.thisStamp + ', X:' + event.nativeEvent.locationX + ', Y' + event.nativeEvent.locationY);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome0}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.welcome2}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.welcome3}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.welcome4}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.welcome5}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.welcome6}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.welcome7}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.welcome8}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.welcome9}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.welcome10}>
-          Welcome to React Native!
-        </Text>
+      <View style={styles.container} onTouchStart={this.onTouchStart} onTouchMove={this.onTouchMove} onTouchEnd={this.onTouchEnd}>
       </View>
     );
   }
@@ -53,54 +41,6 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCEF',
-  },
-  welcome0: {
-    flex: 1, justifyContent: 'center', alignItems: 'center',
-    transform: [{rotate: '45deg'}]
-  },
-  welcome1: {
-    flex: 1, justifyContent: 'center', alignItems: 'center',
-    transform: [{rotateX: '45deg'}]
-  },
-  welcome2: {
-    flex: 1, justifyContent: 'center', alignItems: 'center',
-    transform: [{rotateY: '45deg'}]
-  },
-  welcome3: {
-    flex: 1, justifyContent: 'center', alignItems: 'center',
-    transform: [{rotateZ: '45deg'}]
-  },
-  welcome4: {
-    flex: 1, justifyContent: 'center', alignItems: 'center',
-    transform: [{scale: 2}]
-  },
-  welcome5: {
-    flex: 1, justifyContent: 'center', alignItems: 'center',
-    transform: [{scaleX: 2}]
-  },
-  welcome6: {
-    flex: 1, justifyContent: 'center', alignItems: 'center',
-    transform: [{scaleY: 2}]
-  },
-  welcome7: {
-    flex: 1, justifyContent: 'center', alignItems: 'center',
-    transform: [{translateX: 200}]
-  },
-  welcome8: {
-    flex: 1, justifyContent: 'center', alignItems: 'center',
-    transform: [{translateY: 150}]
-  },
-  welcome9: {
-    flex: 1, justifyContent: 'center', alignItems: 'center',
-    transform: [{skewX: '45deg'}]
-  },
-  welcome10: {
-    flex: 1, justifyContent: 'center', alignItems: 'center',
-    transform: [{skewY: '45deg'}]
-  },
-  
-
+  }
 });
