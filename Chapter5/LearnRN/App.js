b62442a9f8c66c82c83149d212113add14b02614
Leon Guo <chineseroyalstar@gmail.com>
2018-09-08 23:39:31 +0800

    第5章 flexbox布局、View、Image与可触摸组件: View组件的阴影与其他视觉效果

--- Chapter5/LearnRN/App.js
+++ Chapter5/LearnRN/App.js
@@ -15,13 +15,15 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.welcome} opacity={0} />
+        {/* <View style={styles.welcome} opacity={0} />
         <View style={styles.welcome} opacity={0.1} />
         <View style={styles.welcome} opacity={0.25} />
         <View style={styles.welcome} opacity={0.5} />
         <View style={styles.welcome} opacity={0.75} />
         <View style={styles.welcome} opacity={1} />
-        <View style={styles.welcome} opacity={5} />
+        <View style={styles.welcome} opacity={5} /> */}
+        <View style={styles.block1} />
+        <View style={styles.block2} />
       </View>
     );
   }
@@ -32,7 +34,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: 'grey'
+    backgroundColor: 'white'
   },
   welcome: {
     width: 50,
@@ -40,5 +42,20 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 25,
     backgroundColor: 'white',
+  },
+  block1: {
+    width: 150,
+    height: 150,
+    borderWidth: 1,
+    backgroundColor: 'black',
+    borderRadius: 25
+  },
+  block2: {
+    width: 150,
+    height: 150,
+    borderWidth: 1,
+    backgroundColor: 'black',
+    borderRadius: 25,
+    elevation: 200 //安卓平台特有样式, 两样式唯一区别, block2有elvvation键值
   }
 });
