8586b45463d4e2088144c3a60771a344a2b67a72
Leon Guo <chineseroyalstar@gmail.com>
2018-09-17 23:57:35 +0800

    第5章 flexbox布局、View、Image与可触摸组件: pointerEvents属性(添加注释)

--- Chapter5/LearnRN/App.js
+++ Chapter5/LearnRN/App.js
@@ -11,6 +11,12 @@ import {StyleSheet, View, Text} from 'react-native';
 
 export default class App extends Component {
  
+  // pointEvents是字符串类型的属性, 它可以取值为none、box-one、box-only、auto.
+  // 当取值为none时,发生在本组件与本组件的子组件上的触摸事件都会交给本组件的父组件处理.
+  // 当取值为box-none时,发生在本组件显示范围内(非本组件的子组件显示范围内)的事件将交由本组件的父组件处理,发生在本组件的子组件显示范围内的触摸事件由子组件处理
+  // 当取值为box-only时,发生在本组件显示范围内的触摸事件将全部由本组件处理(即使触摸事件发生在本组件的子组件显示范围内).
+  // 而auto的行为视组件的不同而不同,并不是所有的子组件都支持box-none和box-only两个值
+  
   constructor(props) {
     super(props);
     this.state = {bigButtonPointerEvents:null};
@@ -25,11 +31,11 @@ export default class App extends Component {
   onSmallButtonPressed() {
     if (this.state.bigButtonPointerEvents === null) {
       console.log('big button will not reponde.');
-      this.setState({bigButtonPointerEvents:'none'});
+      this.setState({bigButtonPointerEvents:'none'}); //改变状态机变量值
       return;
     }
     console.log('big button will response.');
-    this.setState({bigButtonPointerEvents:null});
+    this.setState({bigButtonPointerEvents:null}); //改变状态机变量值
   }
 
   render() {
