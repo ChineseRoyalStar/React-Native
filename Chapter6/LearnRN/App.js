9b07d1133a43144330c74b6a89b2c624463bcd98
Leon Guo <chineseroyalstar@gmail.com>
2018-09-23 00:00:21 +0800

    第6章 Text、TextInput等相关知识: Text组件的嵌套

--- Chapter6/LearnRN/App.js
+++ Chapter6/LearnRN/App.js
@@ -15,10 +15,15 @@ export default class App extends Component {
     let aLongString = "我很长,显示不下怎么办";
     return (
       <View style={styles.container}>
-        <Text style={styles.textStyle} ellipsizeMode='head' numberOfLines={1}>{aLongString}</Text>
-        <Text style={styles.textStyle} ellipsizeMode='middle' numberOfLines={1}>{aLongString}</Text>
-        <Text style={styles.textStyle} ellipsizeMode='tail' numberOfLines={1}>{aLongString}</Text>
-        <Text style={styles.textStyle} ellipsizeMode='clip' numberOfLines={1}>{aLongString}</Text>
+        <Text style={styles.font20}>
+          我是20号字体
+            <Text style={styles.boldFont20}>
+            {'\r\n'}我是加粗20号字体
+              <Text styles={styles.balckBoldFont20}>
+                {'\r\n'}我是加粗黑色20号字体
+              </Text>
+            </Text>
+        </Text>
       </View>
     );
   }
@@ -30,12 +35,14 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     alignItems: 'center',
   },
-  textStyle: {
-    height: 45,
-    width: 200,
-    fontSize: 30,
-    backgroundColor: 'gray',
-    textAlign: 'center',
-    margin: 5
+  font20: {
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  boldFont20: {
+    fontWeight:'bold'
+  },
+  balckBoldFont20: {
+    color: 'black'
   }
 });
