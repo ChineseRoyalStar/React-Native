6bf641b8d17858360f9d83290916476d296426e4
Leon Guo <chineseroyalstar@gmail.com>
2018-10-24 00:23:08 +0800

    第11章 手势识别: 单点手势---单点任意方向拉动选择界面

--- Chapter11/LearnRN/App.js
+++ Chapter11/LearnRN/App.js
@@ -7,9 +7,17 @@
  */
 
 import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, Dimensions, PanResponder, Platform, View} from 'react-native';
+import {AppRegistry, StyleSheet, Dimensions, PanResponder, View, Image} from 'react-native';
 
 var totalWidth = Dimensions.get('window').width;
+let totalHeight = Dimensions.get('window').height;
+let startHeight = totalHeight - totalWidth/2 + 5;
+let PixelRatio = require('PixelRatio');
+let pixelRatio = PixelRatio.get();
+let centerX = 5 + (totalWidth/2-10)/2;
+let centerY = startHeight + (totalWidth/2-10)/2;
+var unTouchedIcon = require('./cry.png');
+var touchedIcon = require('./smile.png');
 
 export default class App extends Component {
   constructor(props) {
@@ -17,7 +25,12 @@ export default class App extends Component {
     this.watcher = null;
     this.startX = 0;
     this.state = {
-      leftPoint: 1
+      imageIcon: unTouchedIcon,
+      width: (totalWidth/2-10)/4,
+      height: (totalWidth/2-10)/4,
+      left: ((totalWidth/2-10) - ((totalWidth/2-10)/4))/2,
+      top: ((totalWidth/2-10) - ((totalWidth/2-10)/4))/2,
+      borderRadius: (totalWidth/2-10)/8
     };
     this._onPanResponderGrant = this._onPanResponderGrant.bind(this);
     this._onPanResponderMove = this._onPanResponderMove.bind(this);
@@ -34,62 +47,88 @@ export default class App extends Component {
   }
 
   _onPanResponderGrant(e, gestureState) {
-    this.startX = gestureState.x0;
+    let distance = this.caculateDistance(gestureState.x0, gestureState.y0);
+    if (distance < (totalWidth/2-10)/8) {
+      this.dragStart = true;
+      this.setState(()=>{
+        return {imageIcon: touchedIcon};
+      })
+    }
   }
 
   _onPanResponderMove(e, gestureState) {
-    let leftPoint;
-    if (gestureState.moveX < this.startX) {
-      leftPoint = 1;
-    }else {
-      if (gestureState.moveX > totalWidth - 42 - 48 + this.startX) {
-        leftPoint = totalWidth - 42 - 48;
-      }
-      else {
-        leftPoint = gestureState.moveX - this.startX;
-      }
+    if(!this.dragStart) return;
+    let distance = this.caculateDistance(gestureState.moveX, gestureState.moveY);
+    if (distance < (totalWidth/2-10)/8) return;
+    if (distance > (totalWidth/2-10)/2) {
+
     }
+    let left = ((totalWidth/2 - 10) - ((totalWidth/2-10)/4))/2 - distance + (totalWidth/2-10)/8;
+    let top = ((totalWidth/2 - 10) - ((totalWidth/2-10)/4))/2 - distance + (totalWidth/2-10)/8;
+    let width = 2 * distance;
+    let height = 2 * distance;
+    let borderRadius = distance;
     this.setState(()=>{
-      return {leftPoint};
-    });
+      return {left, top, width, height, borderRadius};
+    })
   }
 
   _onPanResponderEnd(e, gestureState) {
-    let leftPoint = 1;
+    let imageIcon = unTouchedIcon;
+    let width = (totalWidth/2-10)/4;
+    let height = (totalWidth/2-10)/4;
+    let left = ((totalWidth/2-10) - ((totalWidth/2-10)/4))/2;
+    let top = ((totalWidth/2-10) - ((totalWidth/2-10)/4))/2;
+    let borderRadius = (totalWidth/2-10)/8;
+    this.dragStart = false;
     this.setState(()=>{
-      return {leftPoint};
-    })
+      return {imageIcon, left, top, width, height, borderRadius};
+    });
   }
+
+  caculateDistance(x,y) {
+    return Math.sqrt(Math.pow((x-centerX),2) + Math.pow((y-centerY),2));
+  }
+
   render() {
     return(
       <View style={styles.container}>
-        <View style={styles.barViewStyle}>
-          <View style={[styles.buttonViewStyle, {left: this.state.leftPoint}]} {...this.watcher.panHandlers}/>
-        </View>
+      <View style={styles.transparentViewStyle} {...this.watcher.panHandlers}>
+          <View style={[styles.shadowViewStyle, {borderRadius: this.state.borderRadius, left: this.state.left, top: this.state.top, width: this.state.width, height: this.state.height}]} />
+          <Image style={styles.touchImageStyle} source={this.state.imageIcon} />
+      </View>
       </View>
     );
   }
 }
 
 var styles = StyleSheet.create({
-  barViewStyle: {
-    width: totalWidth - 40,
-    height: 50,
-    backgroundColor: 'grey',
-    borderRadius: 25,
-    left: 20,
-    top: 50,
-    flexDirection: 'row'
-  },
   container: {
     flex: 1
   },
-  buttonViewStyle: {
-    width: 48,
-    height: 48,
-    borderRadius: 24,
-    backgroundColor: 'white',
-    left: 1,
-    top: 1
+  touchImageStyle: {
+    width: (totalWidth/2-10)/4,
+    height: (totalWidth/2-10)/4,
+    borderRadius: (totalWidth/2-10)/8,
+    position: 'absolute',
+    left: ((totalWidth/2-10) - ((totalWidth/2-10)/4))/2,
+    top: ((totalWidth/2-10) - ((totalWidth/2-10)/4))/2,
+    backgroundColor: 'grey'
+  },
+  shadowViewStyle: {
+    width: (totalWidth/2-10)/4,
+    height: (totalWidth/2-10)/4,
+    left: ((totalWidth/2-10) - ((totalWidth/2-10)/4))/2,
+    top: ((totalWidth/2-10) - ((totalWidth/2-10)/4))/2,
+    backgroundColor: 'blue',
+    borderRadius: (totalWidth/2-10)/8,
+    position: 'absolute'
+  },
+  transparentViewStyle: {
+    width: totalWidth/2 - 10,
+    height: totalWidth/2 - 10,
+    backgroundColor: 'transparent',
+    left: 5, 
+    top: startHeight
   }
 });
