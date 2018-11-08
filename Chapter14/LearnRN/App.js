53a965ad2ee277a75215c57b940f3692c5b35432
Leon Guo <chineseroyalstar@gmail.com>
2018-11-09 01:01:58 +0800

    第14章 应用权限与图库操作: 显示从CameraRoll API得到的图片

--- Chapter14/LearnRN/App.js
+++ Chapter14/LearnRN/App.js
@@ -7,15 +7,24 @@
  */
 
 import React, {Component} from 'react';
-import {AppRegistry, CameraRoll} from 'react-native';
+import {AppRegistry, StyleSheet, Image, View, Text, CameraRoll, TouchableHighlight} from 'react-native';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props) 
+    this.state = {image: null};
+    this.fetchParams = {first: 1};
+    this.nextPic = this.nextPic.bind(this);
+    this.count = 1;
+  }
 
   componentWillMount() {
     this.fetchParams = {first: 50};
     CameraRoll.getPhotos(this.fetchParams).then(
       (data) => {
-        this.getPhotosResults(data)
+        let image = data.edges[0].node.image;
+        this.setState({image});
       }
     ).catch(
       (error) => {
@@ -25,27 +34,13 @@ export default class App extends Component {
     );
   }
 
-  logError(error) {
-    console.log("error:" + error);
-  }
-
-  getPhotosResults(data) {
-    var assets = data.edges;
-    let len = assets.length;
-    let asset;
-    for(let i=0; i<len; i++) {
-      asset = assets[i].node;
-    }
-    console.log(asset);
-    if (!data.page_info.has_next_page) {
-      console.log('all the photo info is got.');
-      return;
-    }
-    this.fetchParams = {fisrt:50, after:data.page_info.end_cursor};
-    console.log('get next 50 photos.');
+  nextPic() {
+    this.count = this.count + 1;
+    let number = this.count;
     CameraRoll.getPhotos(this.fetchParams).then(
       (data) => {
-        this.getPhotosResults(data);
+        let image = data.edges[this.count].node.image;
+        this.setState({image});
       }
     ).catch(
       (error) => {
@@ -55,7 +50,30 @@ export default class App extends Component {
     );
   }
 
-  render(){
-    return null;
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight onPress={this.nextPic}>
+          <Image source={this.state.image} style={styles.imageStyle}/>
+        </TouchableHighlight>
+      </View>
+    );
   }
 }
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  btnStyle: {
+    height: 20,
+    width: 200,
+  },
+  imageStyle: {
+    width: 300,
+    height: 500
+  }
+})
