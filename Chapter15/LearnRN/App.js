8c944a877ca3ee529672e3dac74bd080e9ee1b94
Leon Guo <chineseroyalstar@gmail.com>
2018-11-16 01:09:37 +0800

    第15章 选择器、位置相关和应用状态: AppState API

--- Chapter15/LearnRN/App.js
+++ Chapter15/LearnRN/App.js
@@ -7,40 +7,28 @@
  */
 
 import React, {Component} from 'react';
-import {Slider, View, Text} from 'react-native';
+import {AppState} from 'react-native';
 
 export default class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {aValue: 0};
-    this.onValueChange = this.onValueChange.bind(this);
-    this.onSlidingComplete = this.onSlidingComplete.bind(this);
-  }
-
-  componentDidMount() {
-  }
-
-  onValueChange(aValue) {
-    this.setState({aValue});
-  }
-
-  onSlidingComplete(aValue) {
-    this.setState({aValue});
-    console.log('complete, value:' + aValue);
+  onValueChange(choice, noUse) {
+    this.setState({choice});
   }
 
   componentWillMount() {
-    window.cancelAnimationFrame(this.progressTimer);
+    AppState.addEventListener('change', this._handleAppStateChange.bind(this));
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this._handleAppStateChange);
+  }
+
+  _handleAppStateChange(newState) {
+    console.log('Appstate is:' + newState);
   }
 
   render(){
-    return (
-      <View>
-        <Slider style={{margin:30}} disabled={false} onValueChange={this.onValueChange} onSlidingComplete={this.onSlidingComplete}/>
-        <Text>{'    '}你选择了{this.state.aValue}</Text>
-      </View>
-    );
+    return null;
   }
 }
 
